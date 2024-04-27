33a7a7343a26041933faff1d61c99a3d08cda1e7
antaragharat99 <52412284+antaragharat99@users.noreply.github.com>
2024-04-27 14:47:24 +0530

    Update nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -15,7 +15,7 @@
             <li class="active"><a href="index.php">Home</a></li>
               <?php if(!isset($_SESSION['email'])): ?>
             <li><a href="login.php">Login</a></li>
-            <li><a href="Register.php">Register</a></li>
+            <li><a href="register.php">Register</a></li>
               <?php else: ?>
             <li><a href="logout.php">logout</a></li>
               <?php endif;?>
